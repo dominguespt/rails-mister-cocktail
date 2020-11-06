7d6460841a030cb3a76fe233c0cb7e9808cf570d
luis domingues <dominguesfluis@gmail.com>
2020-11-06 12:18:56 +0000

    Add dotenv - Protect my secret data in .env file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,11 @@ require("channels")
 
 import 'bootstrap';
 
+document.addEventListener("turbolinks:load", function () {
+  // Add tooltips to elements with the data-toggle "tooltip"
+  $('[data-toggle="tooltip"]').tooltip();
+})
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,7 @@
 class Ingredient < ApplicationRecord
-  has_many :doses
+  has_many :doses #, dependent: :restrict_with_error
 
   validates :name, presence: true, uniqueness: true
+
+  scope :ordered, -> { order(name: :asc) }
 end
